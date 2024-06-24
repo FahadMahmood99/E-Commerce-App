3a78451335c3187797b474337aeebd2023c9401a
Fahad Mahmood <fahadfgh14@gmail.com>
2024-06-24 22:21:21 +0500

    Registration And Login Added

---
+++ app/src/main/java/com/example/ecommerce/LoginActivity.java
@@ -0,0 +1,109 @@
+package com.example.ecommerce;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        auth=FirebaseAuth.getInstance();
+
+
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+
+
+    }
+
+    public void signin(View view) {
+
+
+        String useremail = email.getText().toString();
+        String userpassword = password.getText().toString();
+
+        if(TextUtils.isEmpty(useremail))
+        {
+            Toast.makeText(this, "Enter E-mail Address", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(userpassword))
+
+        {
+            Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if(userpassword.length()<6)
+        {
+            Toast.makeText(this, "Password too short!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.signInWithEmailAndPassword(useremail, userpassword)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Successfully Logged In", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
+                                Toast.makeText(LoginActivity.this, "Incorrect Email or Password", Toast.LENGTH_SHORT).show();
+                            } catch (FirebaseAuthInvalidUserException e) {
+                                Toast.makeText(LoginActivity.this, "No account with this email", Toast.LENGTH_SHORT).show();
+                            } catch (Exception e) {
+                                Toast.makeText(LoginActivity.this, "Authentication failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+
+    }
+
+
+    public void signup(View view) {
+        startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/ecommerce/RegistrationActivity.java
@@ -0,0 +1,109 @@
+package com.example.ecommerce;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    EditText name;
+    EditText email;
+    EditText password;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registration);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        auth=FirebaseAuth.getInstance();
+
+//        if(auth.getCurrentUser()!=null)
+//        {
+//
+//        }
+
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+
+    }
+
+    public void signup(View view) {
+
+        String username = name.getText().toString();
+        String useremail = email.getText().toString();
+        String userpassword = password.getText().toString();
+
+
+        if(TextUtils.isEmpty(username))
+        {
+            Toast.makeText(this, "Enter name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if(TextUtils.isEmpty(useremail))
+        {
+            Toast.makeText(this, "Enter E-mail Address", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if(TextUtils.isEmpty(userpassword))
+        {
+            Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if(userpassword.length()<6)
+        {
+            Toast.makeText(this, "Password too short!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(useremail,userpassword)
+                        .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()){
+                                    Toast.makeText(RegistrationActivity.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(RegistrationActivity.this, "Registration Failed"+task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+    }
+
+
+    public void signin(View view) {
+        startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+    }
+}
